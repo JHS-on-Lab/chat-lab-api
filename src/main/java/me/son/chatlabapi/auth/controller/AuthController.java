a0bd82969e16fb3c78f87564bde514db3b713a77
jonghyun <jhs.on.lab@gmail.com>
2026-02-28 16:25:07 +0900

    feat: enhance login authentication

--- src/main/java/me/son/chatlabapi/auth/controller/AuthController.java
+++ src/main/java/me/son/chatlabapi/auth/controller/AuthController.java
@@ -1,14 +1,16 @@
 package me.son.chatlabapi.auth.controller;
 
 import jakarta.servlet.http.HttpServletResponse;
+
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+
 import me.son.chatlabapi.auth.domain.service.AuthService;
 import me.son.chatlabapi.auth.dto.AuthRequestDto;
-import me.son.chatlabapi.auth.dto.AuthResponseDto;
 import me.son.chatlabapi.auth.dto.JwtDto;
 import me.son.chatlabapi.global.response.ApiResponse;
 import me.son.chatlabapi.global.security.CustomUserDetails;
+
 import org.springframework.web.bind.annotation.*;
 
 import static me.son.chatlabapi.global.util.CookieUtil.addHttpOnlyCookie;
@@ -21,23 +23,19 @@ public class AuthController {
     private final AuthService authService;
 
     @PostMapping("/sign-in")
-    public ApiResponse<AuthResponseDto> signIn(@RequestBody AuthRequestDto request, HttpServletResponse response) {
-        log.info("signIn request: {}", request.toString());
+    public ApiResponse<String> signIn(@RequestBody AuthRequestDto request, HttpServletResponse response) {
+        log.info("signIn username: {}", request.getUsername());
         CustomUserDetails user = authService.authenticate(request.getUsername(), request.getPassword());
 
         JwtDto tokens = authService.createTokensByUser(user.getId(), user.getUsername(), user.getRole());
         // Refresh Token 은 HTTP Only Cookie 저장
         addHttpOnlyCookie(response, "refreshToken", tokens.getRefreshToken());
 
-        return ApiResponse.success(AuthResponseDto.builder()
-                .accessToken(tokens.getAccessToken())
-                .username(user.getUsername())
-                .build())
-                ;
+        return ApiResponse.success(tokens.getAccessToken());
     }
 
     @PostMapping("/reissue")
-    public ApiResponse<AuthResponseDto> reissue(@CookieValue(value = "refreshToken") String refreshToken, HttpServletResponse response) {
+    public ApiResponse<String> reissue(@CookieValue(value = "refreshToken") String refreshToken, HttpServletResponse response) {
         log.info("reissue refreshToken: {}", refreshToken);
         // refresh token 유효성 검사
         CustomUserDetails user = authService.validateToken(refreshToken);
@@ -46,11 +44,7 @@ public class AuthController {
         // Refresh Token 은 HTTP Only Cookie 저장
         addHttpOnlyCookie(response, "refreshToken", tokens.getRefreshToken());
 
-        return ApiResponse.success(AuthResponseDto.builder()
-                .accessToken(tokens.getAccessToken())
-                .username(user.getUsername())
-                .build())
-                ;
+        return ApiResponse.success(tokens.getAccessToken());
     }
 
 }
